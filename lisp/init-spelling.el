0dafcd19bcee5dd0e43646b69084867f40dc20c1
wolfeius <ajcastany@gmail.com>
2022-09-23 15:51:53 +0100

    ADD: flyspell for windows (requires hunspell)

--- lisp/init-spelling.el
+++ lisp/init-spelling.el
@@ -4,13 +4,21 @@
 
 (require 'ispell)
 
-(when (executable-find ispell-program-name)
-  ;; Add spell-checking in comments for all programming language modes
-  (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+(when (string-equal system-type 'windows-nt)
+  (setq ispell-program-name "~/hunspell-1.3.2-3-w32-bin/bin/hunspell.exe"))
 
-  (with-eval-after-load 'flyspell
-    (define-key flyspell-mode-map (kbd "C-;") nil)
-    (add-to-list 'flyspell-prog-text-faces 'nxml-text-face)))
+;; (cond (string-equal system-type "windows-nt") (setq ispell-program-name "~/hunspell-1.3.2-3-w32-bin/bin/hunspell.exe"))
+
+;; (cond
+;;  (( string-equal system-type "windows-nt")
+;;   (setq ispell-program-name "~/hunspell-1.3.2-3-w32-bin/bin/hunspell.exe")))
+
+;; Add spell-checking in comments for all programming language modes
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+
+(with-eval-after-load 'flyspell
+  (define-key flyspell-mode-map (kbd "C-;") nil)
+  (add-to-list 'flyspell-prog-text-faces 'nxml-text-face))
 
 (provide 'init-spelling)
 ;;; init-spelling.el ends here
